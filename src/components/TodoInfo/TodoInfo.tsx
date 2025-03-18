58dde470a6c9d251547382fa9fcfc86a3d989583
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-03-18 18:44:13 +0200

    task solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -10,9 +10,7 @@ export const TodoInfo: React.FC<Props> = ({ todo }) => {
   return (
     <article
       data-id={String(todo.id)}
-      className={classNames('TodoInfo', {
-        'TodoInfo--completed': todo.completed,
-      })}
+      className={classNames(todo.completed? 'TodoInfo TodoInfo--completed' : 'TodoInfo')}
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
